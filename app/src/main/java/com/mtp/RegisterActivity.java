29cf274a3634b4b59ccaec43b1e492df1832b8b2
solam <sssolammm@hotmail.com>
2019-02-25 10:19:47 +0100

    login and register before facebook

--- app/src/main/java/com/mtp/RegisterActivity.java
+++ app/src/main/java/com/mtp/RegisterActivity.java
@@ -2,12 +2,15 @@ package com.mtp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Toast;
+
+import static com.mtp.MainActivity.hideSoftKeyboard;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -19,6 +22,14 @@ public class RegisterActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Close keyboard on background click
+        ConstraintLayout constrainLayout= (ConstraintLayout) findViewById(R.id.backgroundWallpaper);
+        constrainLayout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                hideSoftKeyboard(RegisterActivity.this);
+            }
+        });
     }
 
     @Override
