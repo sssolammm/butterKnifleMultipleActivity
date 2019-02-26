8a68fba50424bb37cee5567515edcb32ca51aa1d
David Benito <sssolammm@hotmail.com>
2019-02-27 00:55:59 +0100

    bottom bar navigation

--- app/src/main/java/com/mtp/LocationActivity.java
+++ app/src/main/java/com/mtp/LocationActivity.java
@@ -1,12 +1,9 @@
 package com.mtp;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -33,7 +30,7 @@ public class LocationActivity extends AppCompatActivity implements OnMapReadyCal
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu, menu);
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
         return true;
     }
 
